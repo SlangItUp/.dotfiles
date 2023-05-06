13812b16716ffeac0c20154cdd360e8395f085dc
SlangItUp <anything@slangitup.xyz>
2023-05-06 19:38:52 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+A minimal window manager experience that just works
+
+Arch GNU/Linux based system with wayland
+
+The following programs are being used:
+
+ - Window manager 
+    - Sway : No borders, config at sway/config
+ - Terminal Emulator:
+    - foot : config at foot/foot.ini
+ - Application launcher
+    - wofi : wayland rofi replacement, config at wofi/
+ - Status Bar
+    - swaybar : hidden by default, press $MOD + CTRL to show bar, config at sway/status.sh
+ -  Shell
+    - bash : config at bashrc
+
+Required Font:
+JetBrains mono nerd
+
+Install on arch:
+```bash
+$ sudo pacman -S ttf-jetbrains-mono-nerd
+```
---
+++ sway/status.sh
@@ -0,0 +1,18 @@
+# This script is called by sway config file at .confgi/sway/config
+
+# date block, format : day date mont
+date_block=`date +%A-%B-%d`
+# time block, format : HOUR:MINUTE
+time_block=`date +%H:%M`
+
+# battery block
+battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
+battery_status=$(cat /sys/class/power_supply/BAT0/status)
+# network block
+
+# audio block
+audio_volume=$(amixer get Master | grep -Eom1 '[0-9]+%')
+
+#weather block
+# weather=$(curl -Ss 'https://wttr.in/Kathmandu?0&T&Q&format=1')
+echo  ' |' $battery_level'%   '$battery_status '| 󰕾 '$audio_volume '|  ' $date_block '| ' $time_block '|  '
